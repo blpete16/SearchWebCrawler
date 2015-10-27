81b435f8a2a5e8356c884163ed4d7aac43ae2aec
Brian Peterson <bpete16@lsu.edu>
2015-10-26 21:31:57 -0500

    changed web crawler

--- Attempt1.py
+++ Attempt1.py
@@ -7,6 +7,7 @@ from Utilities import *
 from bs4 import BeautifulSoup
 from twisted.web.client import getPage
 from twisted.internet import reactor
+from twisted.python import log
 import sqlite3
 import time
 import thread
@@ -18,6 +19,9 @@ url_buffer = []
 BASEFILE = "baseurls.dat"
 DATABASEFILE = "index.db"
 terminate = False
+
+defcount= 0
+MAXDEFCOUNT = 100
 
 def makeconn():
     return sqlite3.connect(DATABASEFILE)
@@ -31,9 +35,13 @@ def GetAuthor(soup):
 def GrabLink(url, atag):
     try:
         val = atag.attrs["href"]
+        if(val is None):
+            return None
         if(val.startswith("mailto")):
             return None
-        if(".edu" in val) or(".com" in val) or (".org" in val):
+        if(".com" in val or ".org" in val):
+            return None
+        if(".edu" in val):
             pass
         else:
             val = url + val
@@ -43,8 +51,8 @@ def GrabLink(url, atag):
         return None
 
 def modifyDatabase(url, author):
-    if(author is None or author ==""):
-        return
+    global defcount
+    defcount = defcount + 1
     conn = makeconn()
     c = conn.cursor()
     c.execute('SELECT ID FROM urls WHERE url="'+url+'"')
@@ -73,20 +81,31 @@ def extractPageInfo(html, url, hop=0):
     for atag in atags:
         linktext = GrabLink(url, atag)
         if(not linktext is None):
-            dbgprint(linktext)
-            #CrawlPage(linktext, hop - 1)
+            CrawlPage(linktext, hop - 1)
     
-def CrawlPage(url, hop=0):
+def CrawlPage(url, hop=0, force=False):
+    global defcount
     if(hop < 0):return
     if(url is None):return
-    urlvalascii = url.encode('ascii','ignore')
-    deferred = getPage(urlvalascii)
-    dbgprint("before deferred add")
-    deferred.addCallback(extractPageInfo, url, hop=hop)
+    if((not UrlInDB(url) or force) and ( defcount <  MAXDEFCOUNT)):
+        urlvalascii = url.encode('ascii','ignore')
+        deferred = getPage(urlvalascii)
+        dbgprint("before deferred add")
+        deferred.addCallback(extractPageInfo, url, hop=hop)
+        deferred.addErrback(log.err)
         
+def UrlInDB(url):
+    conn = makeconn()
+    c = conn.cursor()
+    c.execute('SELECT id FROM urls WHERE url="' + url + '"')
+    rows = c.fetchall()
+    if(len(rows) > 0):
+        return True
+    return False
     
 def Setup():
     if(not os.path.isfile(DATABASEFILE)):
+        dbgprint("SETTING UP!!")
         conn = makeconn()
         c = conn.cursor()
         c.execute('''CREATE TABLE urls 
@@ -111,7 +130,8 @@ def PrimeDeferreds():
         urlval = row[1]
         urlvalascii = urlval.encode('ascii','ignore')
         dbgprint("Before crawlpage: " + urlvalascii)
-        CrawlPage(urlvalascii, hop=0)
+        CrawlPage(urlvalascii, hop=2, force=True)
+
 
 def monitorThread():
     while(True):
